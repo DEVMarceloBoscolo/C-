7d884564c3005e084ddd168ff8c333d17987fefb
MarceloBoscolo <marceloboscolo215@gmail.com>
2025-10-06 16:41:32 -0300

    Add files via upload

---
+++ Contagem de 1 a 30.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("de 1 a 30:");
+
+
+for (int i = 1; i <= 30; i++)
+
+    if (i % 2 != 0)  
+    {
+        Console.WriteLine(i);
+    }
+/* se tirarmos o if teremos so a contagem*/
---
+++ verificador de maior e menor de idade.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("VERIFICADOR DE IDADE:  ");
+
+Console.Write("digite uma idade: ");
+int n1 = int.Parse(Console.ReadLine());
+
+if (n1>=18)
+{
+    Console.WriteLine("      \n         ====Maior de Idade====         ");
+}
+else
+{
+    Console.WriteLine("       \n        ==== Menor de Idade ====         ");
+}
