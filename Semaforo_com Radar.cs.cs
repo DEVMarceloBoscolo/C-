980dad823a2f869756da422775f160e71065ca58
MarceloBoscolo <marceloboscolo215@gmail.com>
2025-10-06 14:13:13 -0300

    Semaforo_radar_semaforico

---
+++ Semaforo_com Radar.cs.cs
@@ -0,0 +1,39 @@
+﻿Random random = new Random();
+int multa = 0;
+for (int ciclo = 1; ciclo <= 5; ciclo++)
+{
+    Console.WriteLine($"----Ciclo {ciclo} ----");
+
+
+    int carro = random.Next(2);
+    
+
+    // verde
+    Console.WriteLine("sinal verde por 5 s.");
+    Thread.Sleep(5000);
+
+    // amarelo
+    Console.WriteLine("sinal amarelo por 2 s.");
+    if (carro == 1)
+    {
+        multa++;
+    }
+    else
+  
+    Thread.Sleep(2000);
+
+    // vermelho
+    Console.WriteLine("sinal vermelho por 5 s.");
+    if (carro == 1)
+    {
+        multa++;
+    }
+    else
+    Thread.Sleep(5000);
+
+    Console.WriteLine("\n--------------------------\n");
+}
+            
+Console.WriteLine($"numero de multas:------\n{multa}");
+    
+    
